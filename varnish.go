c9a520d503f4a2f0533943ae82986a05f89d6608
Stuart Fox <sfox@xmatters.com>
2016-03-24 15:12:13 -0700

    Stuff

--- varnish.go
+++ varnish.go
@@ -3,61 +3,93 @@ package varnish
 // varnish.go
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "os/exec"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
-    "github.com/influxdata/telegraf"
-    "github.com/influxdata/telegraf/plugins/inputs"
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
 type Varnish struct {
-    Ok bool
+	Stats []string `toml:"stats"`
 }
 
 func (s *Varnish) Description() string {
-    return "a plugin to collect stats from varnish"
+	return "a plugin to collect stats from varnish"
 }
 
+var varnishSampleConfig = `
+  ## By default, telegraf gather stats for 3 metric points.
+  ## Setting stats will remove the defaults
+  stats = ['MAIN.cache_hit', 'MAIN.cache_miss', 'MAIN.uptime']
+`
+
 func (s *Varnish) SampleConfig() string {
-    return "ok = true # indicate if everything is fine"
+	return varnishSampleConfig
+}
+
+func stringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
 
 func (s *Varnish) Gather(acc telegraf.Accumulator) error {
 
-    cmdName := "/usr/bin/varnishstat"
-    cmdArgs := []string{"-1"}
+	var stats []string
+	sections := []string{"LCK", "MAIN", "MEMPOOL", "MGT", "SMA", "VBE"}
+	fmt.Fprintln(os.Stderr, sections)
 
-    cmd := exec.Command(cmdName, cmdArgs...)
-    cmdReader, err := cmd.StdoutPipe()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-        os.Exit(1)
-    }
+	if len(s.Stats) == 0 {
+		stats = []string{"MAIN.cache_hit", "MAIN.cache_miss", "MAIN.uptime"}
+	} else {
+		stats = s.Stats
+	}
 
-    scanner := bufio.NewScanner(cmdReader)
-    go func() {
-        for scanner.Scan() {
-            fmt.Printf("stat is | %s\n", scanner.Text())
-        }
-    }()
+	cmdName := "/usr/bin/varnishstat"
+	cmdArgs := []string{"-1"}
 
-    err = cmd.Start()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
-        os.Exit(1)
-    }
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
+		os.Exit(1)
+	}
 
-    err = cmd.Wait()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-        os.Exit(1)
-    }
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			stat_line := strings.Fields(scanner.Text())
+			if stringInSlice(stat_line[0], stats) {
+				tmp := strings.Split(stat_line[0], ".")
+				subsect := tmp[1]
+				tags := map[string]string{}
+				tags["name"] = tmp
+			}
+		}
+	}()
 
-    return nil
+	err = cmd.Start()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
+		os.Exit(1)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
+		os.Exit(1)
+	}
+
+	return nil
 }
 
 func init() {
-    inputs.Add("varnish", func() telegraf.Input { return &Varnish{} })
+	inputs.Add("varnish", func() telegraf.Input { return &Varnish{} })
 }
