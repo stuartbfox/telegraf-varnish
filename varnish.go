834d5249525bf1f388d9bb88edba6281718521ee
Robin Percy <rpercy@xmatters.com>
2016-03-27 19:08:31 -0700

    Accumulating one line per section.
    - Changed the call to varnishstat to be synchronous, in order to remove
      race condition
    - Grouping metrics into sections based on the prefix of the line name
    - Making one call to AddFields for each section
    - Differentiating sections using tags

--- varnish.go
+++ varnish.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/influxdata/telegraf"
@@ -43,48 +44,74 @@ func stringInSlice(str string, list []string) bool {
 
 func (s *Varnish) Gather(acc telegraf.Accumulator) error {
 
-	var stats []string
+	sections := []string{"LCK", "MAIN", "MEMPOOL", "MGT", "SMA", "VBE"}
+	stats := []string{"MAIN.cache_hit", "MAIN.cache_miss", "MAIN.uptime",
+		"LCK.lru.locks", "LCK.smp.destroy",
+		"MEMPOOL.sess1.randry", "MEMPOOL.sess1.surplus",
+		"SMA.s0.c_freed"}
+	statsFilter := make(map[string]bool)
 
-	if len(s.Stats) == 0 {
-		stats = []string{"MAIN.cache_hit", "MAIN.cache_miss", "MAIN.uptime"}
-	} else {
-		stats = s.Stats
+	for _, s := range stats {
+		statsFilter[s] = true
+	}
+
+	sectionMap := make(map[string]map[string]interface{})
+
+	for _, s := range sections {
+		sectionMap[s] = make(map[string]interface{})
 	}
 
 	cmdName := "/usr/bin/varnishstat"
 	cmdArgs := []string{"-1"}
 
 	cmd := exec.Command(cmdName, cmdArgs...)
-	cmdReader, err := cmd.StdoutPipe()
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
-		os.Exit(1)
+		return fmt.Errorf("error running varnishstat: %s", err)
 	}
 
-	scanner := bufio.NewScanner(cmdReader)
-	go func() {
-		for scanner.Scan() {
-			stat_line := strings.Fields(scanner.Text())
-			if stringInSlice(stat_line[0], stats) {
-				tmp := strings.Split(stat_line[0], ".")
-				sect := tmp[0]
-				subsect := tmp[1]
-				val := stat_line[1]
+	scanner := bufio.NewScanner(&out)
+	for scanner.Scan() {
+		cols := strings.Fields(scanner.Text())
+		if len(cols) < 2 {
+			continue
+		}
+		if !strings.Contains(cols[0], ".") {
+			continue
+		}
+
+		metric := cols[0]
+		value := cols[1]
+
+		if !statsFilter[metric] {
+			continue
+		}
+
+		parts := strings.SplitN(metric, ".", 2)
+		section := parts[0]
+		field := parts[1]
+
+		// Only add the sections we care about
+		if _, ok := sectionMap[section]; ok {
+			sectionMap[section][field], err = strconv.Atoi(value)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Expected a numeric vlaue for %s = %v\n",
+					metric, value)
 			}
 		}
-		acc.AddFields("varnish", fields, tags)
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
-		os.Exit(1)
 	}
 
-	err = cmd.Wait()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-		os.Exit(1)
+	for section, fields := range sectionMap {
+		tags := map[string]string{
+			"section": section,
+		}
+		if len(fields) == 0 {
+			continue
+		}
+
+		acc.AddFields("varnish", fields, tags)
 	}
 
 	return nil
